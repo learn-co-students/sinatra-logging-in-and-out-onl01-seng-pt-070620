f5265be2b72cb9588305c51f0e1d69144b95a496
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-09-22 14:18:39 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+    def self.current_user(session)
+        @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        if @current_user.id == session[:user_id]
+            return true
+        else
+            return false
+        end
+    end
 end
