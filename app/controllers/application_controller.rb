6bbeeaf67e19dbaa515ee26d035c5bc8e06439c8
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-10-05 14:15:05 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,27 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  # post route processes post request in index.erb
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end 
+      erb :error
   end
 
   get '/account' do
-
+    if !User.is_logged_in?(session)
+      erb :error
+    else 
+      erb :account
+    end 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+    def self.current_user(session)
+        @current_user = User.find_by(id: session[:user_id])
+        @current_user
+    end 
+
+    def self.is_logged_in?(session)
+        if session[:user_id] == @current_user.id
+          true
+        else 
+          false
+        end
+        
+    end 
+
 end
